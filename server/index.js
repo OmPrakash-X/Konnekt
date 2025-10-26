e00e1f427e158f19574635990457a5875463b5d6
OmPrakash-X <omnayak984@gmail.com>
2025-10-26 08:18:08 +0530

    Fixed Minor bugs

--- server/index.js
+++ server/index.js
@@ -40,7 +40,6 @@ app.use(
     origin: process.env.FRONTEND_URL || "http://localhost:5173",
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
-    allowedHeaders: ['Content-Type', 'Authorization']
   })
 );
 
